4d8599a2a17e6dc45406e0a40df799866544ef2a
Kaiwa-Jun <kj.11235813213455@gmail.com>
2025-01-19 01:04:34 +0900

    Add intentional errors to test CI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+  def DoSomethingBAD
+    puts "Rubocop will hate this!"
+  end
 end
---
+++ app/models/sample_spec.rb
@@ -0,0 +1,7 @@
+require "rails_helper"
+
+RSpec.describe "Sample Test" do
+  it "fails intentionally" do
+    expect(1).to eq(2)  # ここでわざと失敗
+  end
+end
